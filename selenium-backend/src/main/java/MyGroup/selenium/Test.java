b7a9d8bdb1be9da462bc2fcafd7807a9bb4cba95
tsvetlena <tsvetlena@abv.bg>
2020-10-23 12:12:45 +0300

    Selenium project

---
+++ selenium-backend/src/main/java/MyGroup/selenium/Test.java
@@ -0,0 +1,34 @@
+package MyGroup.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Test {
+	
+	WebDriver driver = null;
+	
+	@BeforeTest
+
+	 {
+		
+		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://finance.yahoo.com/");
+		
+		WebElement accept = driver.findElement(By.name("agree"));
+		accept.click();
+		
+		WebElement signinBut = driver.findElement(By.id("header-signin-link"));
+		signinBut.click();
+		
+		
+		
+		driver.close();
+		
+		
+	}
+
+}
